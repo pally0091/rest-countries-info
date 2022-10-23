68b50cd2c1c29187ebaff3d8010dd34d89ef5e23
Debashis Roy <goodquote39@hotmail.com>
2022-10-23 20:11:51 +0600

    about added

--- src/componens/About.js
+++ src/componens/About.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
-        <div>
-            <h1> this is about</h1>
-        </div>
+        <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+    
+	
+	<div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+	
+
+		<div className="p-4 md:p-12 text-center lg:text-left">
+			
+			<div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" ></div>
+			
+			<h1 className="text-3xl font-bold pt-8 lg:pt-0">Debashis Roy</h1>
+			<div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+			<p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"> What you do</p>
+			
+			<p className="pt-8 text-sm">Totally optional short description about yourself, what you do and so on.</p>
+
+
+			<div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
+				
+				<Link to=''>Facebook</Link>
+				<Link to=''>GitHub</Link>
+				<Link to=''>LinkedIn</Link>
+			</div>
+			
+			
+
+		</div>
+
+	</div>
+	
+	
+	<div className="w-full lg:w-2/5">
+		
+		<img src="https://source.unsplash.com/MP0IUfwrn0A" className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"/>
+	</div>
+	
+</div>
     );
 };
 
